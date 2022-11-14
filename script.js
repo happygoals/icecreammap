c65d89a9728984639ee6bb9444d1308b8328ff64
hryu <hryu@cirrusabs.com>
2022-11-14 17:36:49 -0500

    Update the map style and add a basic setting of the map (Fort Wayne with a marker)

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let map;
 let fortwayneLatLong = { lat: 41.093842, lng: -85.139236 };
+let ohFiveScoopShop = { lat: 41.1265868, lng: -85.1223213 }; 
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-      center: fortwayneLatLong,
-    zoom: 11,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: fortwayneLatLong,
+        zoom: 11,
+        mapId: "45cf3ebca9440228",
+    });
+
+    const marker = new google.maps.Marker({
+        position: ohFiveScoopShop,
+        label: "oh Five Scoop Shop",
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        map: map,
+    });
+
 }
 
-window.initMap = initMap;
+function addMarkerToFortWayne() {
+    const marker = new google.maps.Marker({
+        position: ohFiveScoopShop,
+        label: "oh Five Scoop Shop",
+        map: map,
+    });
+}
+
